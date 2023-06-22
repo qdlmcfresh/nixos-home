b78439f95bb8c9013df2dbeba55cfdf429bf40b8
qdlmcfresh <qdlmcfresh@gmail.com>
2023-06-22 20:35:55 +0200

    Add vscode

--- home/default.nix
+++ home/default.nix
@@ -3,6 +3,7 @@
 {
 
   imports = [
+    ./programs
   ];
 
   # Home Manager needs a bit of information about you and the
---
+++ home/programs/vscode.nix
@@ -0,0 +1,29 @@
+{ 
+  config,
+  pkgs,
+  home-manager,
+  ... 
+}: 
+
+{
+
+  # if use vscode in wayland, uncomment this line
+  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  programs.vscode = {
+    enable = true;
+    userSettings = {
+      "editor.renderWhitespace" = "all";
+      "editor.rulers" = [ 80 120 ];
+      "telemetry.enableTelemetry" = false;
+      "telemetry.enableCrashReporter" = false;
+      "files.exclude" = { "**/node_modules/**" = true; };
+      "editor.formatOnSave" = false;
+      "breadcrumbs.enabled" = true;
+      "editor.useTabStops" = false;
+      "editor.lineHeight" = 20;
+      "workbench.fontAliasing" = "antialiased";
+      "files.trimTrailingWhitespace" = true;
+      "editor.minimap.enabled" = false;
+      "workbench.editor.enablePreview" = false;
+  };
+}
