4e1562a4968dd2208a40e14ca18bc70b6c8e2c31
qdlmcfresh <qdlmcfresh@gmail.com>
2023-09-25 21:24:29 +0200

    nixos-hardware for common-hidpi module

--- flake.nix
+++ flake.nix
@@ -6,14 +6,15 @@
   };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:leonm1/nixos-hardware";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    leonm1-hardware.url = "github:leonm1/nixos-hardware";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     bw-key.url = "github:qdlmcfresh/bw-key";
     bw-key.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, nixos-hardware, home-manager, nix-vscode-extensions, bw-key, ... }:
+  outputs = inputs@{ self, nixpkgs, nixos-hardware, leonm1-hardware, home-manager, nix-vscode-extensions, bw-key, ... }:
     {
       nixosConfigurations = {
         nixos-vmware = nixpkgs.lib.nixosSystem {
@@ -33,7 +34,8 @@
         surface-book = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            nixos-hardware.nixosModules.microsoft-surface-common
+            leonm1-hardware.nixosModules.microsoft-surface-common
+            nixos-hardware.nixosModules.common-hidpi
             ./hosts/surface-book
             home-manager.nixosModules.home-manager
             {
