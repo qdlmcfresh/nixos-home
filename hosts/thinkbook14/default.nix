23a4791bec3d947f0b862d2b28f346529628e4ab
qdlmcfresh <qdlmcfresh@gmail.com>
2023-10-27 13:47:47 +0200

    Add default stuff for thinkbook

---
+++ hosts/thinkbook14/default.nix
@@ -0,0 +1,78 @@
+{ configs, pkgs, ... }:
+
+{
+  imports = [
+    ../../modules/system.nix
+    ../../modules/i3.nix
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot = {
+    enable = true;
+    consoleMode = "auto";
+  };
+
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "thinkbook14";
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.powersave = false;
+
+  system.stateVersion = "23.05";
+
+  nix.buildMachines = [{
+    hostName = "nixos-vmware";
+    system = "x86_64-linux";
+    protocol = "ssh-ng";
+    # if the builder supports building for multiple architectures,
+    # replace the previous line by, e.g.,
+    # systems = ["x86_64-linux" "aarch64-linux"];
+    maxJobs = 1;
+    speedFactor = 2;
+    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+    mandatoryFeatures = [ ];
+  }];
+  nix.distributedBuilds = true;
+
+  services.printing.enable = true;
+
+  services.xserver.videoDrivers = [ "intel" ];
+
+  environment.systemPackages = with pkgs; [
+    networkmanagerapplet
+    xorg.xf86videointel
+    xorg.xbacklight
+  ];
+
+  environment.variables = {
+    MOZ_USE_XINPUT2 = "1";
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+
+  services.upower.enable = true;
+
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
+}
